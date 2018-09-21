659952169d05a41b3376be3314c6c118d47da5df
developer <developer@fireblade>
2018-09-21 15:49:49 +0200

    stop words

--- src/me/repay/test/WordSequenceFinder.java
+++ src/me/repay/test/WordSequenceFinder.java
@@ -3,13 +3,12 @@ package me.repay.test;
 import java.util.Collection;
 
 /**
- * Finds sequences of up to three words in an input text.
+ * Finds sequences of words in an input text.
  * 
  * The result should contain:
  * - Each contained word exactly once except stop words
  * - Sequences of this word followed by the next one or two successors
  * - Sequences should stop at stop words
- * - Stop words are, for instance, "a", "the", "and" or so.
  * 
  * Example:
  * 'This is a text' should return 'This', 'This is', 'is', 'text' in arbitrary order.
@@ -19,6 +18,8 @@ import java.util.Collection;
 public class WordSequenceFinder {
 
 
+	private static final String[] STOP_WORDS = {"a", "and", "the", "you"};
+	
 	public static Collection<String> findWordSequencesIn(String input) {
 		return null;
 	}
